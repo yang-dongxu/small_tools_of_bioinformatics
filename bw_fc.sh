a45b1213b9589decbd8b7fa26e9f006f956a3245
yang-dongxu <yang_dongxu@qq.com>
2021-03-16 17:21:52 +0800

    add a script to transform bigwig singla to the fold of average intensity

---
+++ bw_fc.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+bw=$1
+chrom=$2
+
+project=`basename -s .bw $bw`
+bdg="${project}.bdg"
+bdg_tmp="${bdg}.tmp"
+obw="${project}.fc.bw"
+ave=`bigWigInfo $1 | grep mean | cut -d ":" -f 2 `
+echo $ave
+bigWigToBedGraph $bw $bdg
+awk '{$4=$4/a;print $0}' a="${ave}" $bdg  | sort -k1,1 -k2,2n > $bdg_tmp
+bedGraphToBigWig $bdg_tmp $chrom $obw
+
+rm -f $bdg
+rm -f $bdg_tmp
