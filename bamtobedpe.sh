a2845af71ee3976717be8fb7c3697f4b158fe030
yang-dongxu <yang_dongxu@qq.com>
2021-06-22 19:57:12 +0800

    update bamtobedpe.sh

--- bamtobedpe.sh
+++ bamtobedpe.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
 ibam=$1
-bamToBed -bedpe -i ${ibam} | awk '{if($2<$5) print $1"\t"$2"\t"$6; else print $1"\t"$5"\t"$3}' | sort -k1,1 -k2,2n | uniq 
+bam_tmp="$1".tmp
+samtools sort -@ 10 -n  -o $bam_tmp $ibam 
+bamToBed -bedpe -i ${bam_tmp} | awk '{if($2<$5) print $1"\t"$2"\t"$6; else print $1"\t"$5"\t"$3}' | sort -k1,1 -k2,2n | uniq 
+
+rm ${bam_tmp}
