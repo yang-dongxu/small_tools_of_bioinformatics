de9d24736a73ad65964227378ed87db0e3df70b0
yang-dongxu <yang_dongxu@qq.com>
2021-06-22 20:05:26 +0800

    修改：     bamtobedpe.sh

--- bamtobedpe.sh
+++ bamtobedpe.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 ibam=$1
+threads=$2
 bam_tmp="$1".tmp
-samtools sort -@ 10 -n  -o $bam_tmp $ibam 
+samtools sort -@ ${threads} -n  -o $bam_tmp $ibam 
 bamToBed -bedpe -i ${bam_tmp} | awk '{if($2<$5) print $1"\t"$2"\t"$6; else print $1"\t"$5"\t"$3}' | sort -k1,1 -k2,2n | uniq 
 
 rm ${bam_tmp}
