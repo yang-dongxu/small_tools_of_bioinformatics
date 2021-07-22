9b2afbab16ae3e3010476983abf50c2b0dbf374f
yang-dongxu <yang_dongxu@qq.com>
2021-07-22 11:51:24 +0800

    修改：     bw_fc.sh: small modification

--- bw_fc.sh
+++ bw_fc.sh
@@ -21,8 +21,10 @@ ave=`bigWigInfo $1 | grep mean | cut -d ":" -f 2 `
 
 echo $ave,$scale
 bigWigToBedGraph $bw $bdg
+echo "generate bdg.tmp..."
 awk '{$4=$4/a*b;print $0}' a="${ave}"  b="${scale}" $bdg  | sort -k1,1 -k2,2n > $bdg_tmp
+echo "generate new bigwig .."
 bedGraphToBigWig $bdg_tmp $chrom $obw
-
+echo "process over, clean up!"
 rm -f $bdg
 rm -f $bdg_tmp
